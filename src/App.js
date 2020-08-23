bbd950803fd7e31d1e06522afd505922ac6e3e56
Szymon <szymqwerty@gmail.com>
2020-08-23 13:40:02 +0200

    Add missing dependencies with main app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import UsersContainer from './users/containers/UsersContainers';
+import CalculatorContainer from './calculator/containers/CalculatorContainer';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/calculator">Money Calculator</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/users">
+            <UsersContainer />
+          </Route>
+          <Route path="/calculator">
+            <CalculatorContainer />
+          </Route>
+          <Route></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
